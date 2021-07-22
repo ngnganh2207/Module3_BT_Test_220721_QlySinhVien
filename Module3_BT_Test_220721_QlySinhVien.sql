29287f6408ba9660feab2ce6a4529924c134f850
ngnganh2207 <ngocanhnguyen2207@gmail.com>
2021-07-22 21:41:18 +0700

    '22.7.21.ver2'

--- Module3_BT_Test_220721_QlySinhVien.sql
+++ Module3_BT_Test_220721_QlySinhVien.sql
@@ -54,16 +54,50 @@ select studentName from students;
 -- C2
 select subjectname from subjects;
 -- C3
---  Chưa hiểu về group by
+select studentID, avg(mark) from marks group by studentID;
 select  studentID, avg(mark) as diemtb from marks group by studentid;
 -- C4
-
+select studentName,mark from students inner join Marks on students.studentID = marks.studentID order by Mark desc limit 4;
 -- C5
--- Chưa hiểu
-select row_number() over(order by mark) as stt, mark from marks;
+-- Tìm hiểu lại hàm row_number() over();
+select mark from marks order by mark desc;
+select ROW_NUMBER() over(order by Mark desc) as SoThuTu, mark from marks order by mark desc;
+select row_number() over( order by mark asc ) as stt, mark from marks order by mark asc;
 -- C6 
 -- Chưa hiểu
-ALTER TABLE `test2`.`subject` 
+ALTER TABLE subjects
 CHANGE COLUMN `subjectname` `subjectname` NVARCHAR(4000);
-
-
+Alter table subjects Modify column subjectName Nvarchar(4000);
+-- C7
+Update subjects set `subjectName`='Day la mon hoc SQL' where (subjectID='1');
+update subjects set `subjectName`='Day la mon hoc Java' where (subjectID='2');
+update subjects set `subjectName`='Day la mon hoc C' where (subjectID='3');
+update subjects set `subjectName`='Day la mon hoc Visual Basic' where (subjectID='4');
+-- C8
+alter table students
+add check (Age>15 and age<50);
+-- C9
+Alter table Marks
+drop foreign key subjectID,
+drop foreign key studentID;
+Alter table classStudent
+drop foreign key studentID,
+drop foreign key classID;
+-- C10
+-- classStudents 5 ,classes 2, marks 9, subject 4, students 5
+-- Câu hiển thị này bị sai, cần xem sai chỗ nào.
+select * from students
+inner join classStudent on students.studentID= classStudent.studentID
+inner join classes on classStudent.classID= classes.classID
+inner join Marks on marks.studentID= students.studentID
+inner join  subjects on subjects.subjectID= marks.subjectID;    
+delete from students where studentID= '1' ; 
+-- C11
+Alter table students
+add column status Bit default 1;
+-- C12
+update students set status= 0 where studentID=1;
+update students set status= 0 where studentID=2;
+update students set status= 0 where studentID=3;
+update students set status= 0 where studentID=4;
+update students set status= 0 where studentID=5;
